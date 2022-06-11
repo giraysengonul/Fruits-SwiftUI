c6accb952529d636d6939854389447098aafe949
hakkicansengonul <coder.9734@gmail.com>
2022-06-11 08:51:44 +0300

    Initial Commit

---
+++ Fruits-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fruits-SwiftUI
+//
+//  Created by hakkı can şengönül on 11.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fruits-SwiftUI/Fruits_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruits_SwiftUIApp.swift
+//  Fruits-SwiftUI
+//
+//  Created by hakkı can şengönül on 11.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Fruits_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
